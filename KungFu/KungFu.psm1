89fc9e270e6be7305367e7eebd5e3beb17b10257
JonatanRek <gamerclassn7@gmail.com>
2024-02-15 08:58:49 +0100

    Fixe

--- KungFu/KungFu.psm1
+++ KungFu/KungFu.psm1
@@ -13,3 +13,4 @@ Get-ChildItem (Split-Path $script:MyInvocation.MyCommand.Path) -Filter '*.ps1' -
 #Get-ChildItem "$(Split-Path $script:MyInvocation.MyCommand.Path)\Public\*" -Filter '*.ps1' -Recurse | ForEach-Object {
 #   Export-ModuleMember -Function $_.BaseName
 #}
+
